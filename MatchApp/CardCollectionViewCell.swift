cbd963ee0e9a76ce9374551c598d730a69603758
Siam300 <89306483+Siam300@users.noreply.github.com>
2025-07-10 21:14:49 +0600

    Added game logic

--- MatchApp/CardCollectionViewCell.swift
+++ MatchApp/CardCollectionViewCell.swift
@@ -24,12 +24,21 @@ class CardCollectionViewCell: UICollectionViewCell {
         frontImageView.image = UIImage(named: card.imageName)
         
         // reset the state of the cell by checking the flip status of the card and then showing the front or the back image view accordingly
-        if card.isFlipped {
+        if card.isMatched == true {
+            backImageView.alpha = 0
+            frontImageView.alpha = 0
+            return
+        } else {
+            backImageView.alpha = 1
+            frontImageView.alpha = 1
+        }
+        
+        if card.isFlipped == true {
             // show the front image view
             flipUP(speed: 0)
         } else {
             // show the back image view
-            flipDown(speed: 0)
+            flipDown(speed: 0, delay: 0)
         }
     }
     
@@ -41,11 +50,23 @@ class CardCollectionViewCell: UICollectionViewCell {
         card?.isFlipped = true
     }
     
-    func flipDown(speed: TimeInterval = 0.3) {
-        // flip down animation
-        UIView.transition(from: frontImageView, to: backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
-        
+    func flipDown(speed: TimeInterval = 0.3, delay: TimeInterval = 0.5) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            // flip down animation
+            UIView.transition(from: self.frontImageView, to: self.backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        }
         // set the status of the card
         card?.isFlipped = false
+    }
+    
+    func remove() {
+        // make the image views invisible
+        backImageView.alpha = 0
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: .curveEaseOut, animations: {
+            self.frontImageView.alpha = 0
+        }, completion: nil)
+        
+
     }
 }
--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     let model = CardModel()
     var cardsArray = [Card]()
+    
+    var firstFlippedCardIndex: IndexPath?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,15 +35,20 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // get a cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+                
+        // return it
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        // configure the state of the cell based on the properties of the card that if represnts
+        let cardCell = cell as? CardCollectionViewCell
         
         // get the card from the card array
         let card = cardsArray[indexPath.row]
         
-        // TODO: FInish configuring cell
-        cell.configureCell(card: card)
-        
-        // return it
-        return cell
+        // FInish configuring cell
+        cardCell?.configureCell(card: card)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -49,15 +56,54 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell
         
         // check the status of the card to determine how to flip it
-        if cell?.card?.isFlipped == false {
+        if cell?.card?.isFlipped == false && cell?.card?.isMatched == false {
             // flip the card up
             cell?.flipUP()
+            
+            // check if this the first card that was flipped or the second card
+            if firstFlippedCardIndex == nil {
+                // this is the first card flipped over
+                firstFlippedCardIndex = indexPath
+            } else {
+                // second card that is flipped
+                
+                
+                // run the comparison logic
+                checkForMatch(indexPath)
+            }
+        }
+    }
+    
+    // MARK: - Game logic methods
+    func checkForMatch(_ secondFlippedCardIndex: IndexPath) {
+        // get the two card objects for the two indices and see if that matches
+        let cardOne = cardsArray[firstFlippedCardIndex!.row]
+        let cardTwo = cardsArray[secondFlippedCardIndex.row]
+        
+        // get the two collection view cells that represnt card one and two
+        let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
+        let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as? CardCollectionViewCell
+        
+        // compare the two cards
+        if cardOne.imageName == cardTwo.imageName {
+            // its amatch
+            
+            // set the status and remove them
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            cardOneCell?.remove()
+            cardTwoCell?.remove()
         } else {
-            // flip the card down
-            cell?.flipDown()
+            // its not a match
+            
+            // flip them back over
+            cardOneCell?.flipDown()
+            cardTwoCell?.flipDown()
         }
         
-        
+        // reset tge firstFlippedCardIndex property
+        firstFlippedCardIndex = nil
     }
 }
 
